989a4073d69d34962dff4b3180a54021d1e3f192
Josebarcellos1810 <pedrohype999@gmail.com>
2026-05-21 22:16:09 -0300

    Add files via upload

---
+++ Aula - 14/script.js
@@ -0,0 +1,21 @@
+var issoeumastring = "isso é uma string"
+var issoeumnumero = 42
+var issoeumbooleano = true
+
+console.log(issoeumastring);
+console.log(issoeumnumero);
+console.log(issoeumbooleano);
+
+var issotambemeumastring = "42";
+
+if (issoeumastring == issotambemeumastring) {
+    console.log("As strigns são iguais");
+} else{console.log("As strigns são iguais"); "As strings são diferentes"
+}
+
+
+if(issoeumnumero == issotambemeumastring) {
+    console.log("O número e a string são iguais");
+} else{
+    console.log("O número e a string são iguais");  "O número e a string são iguais"
+}
